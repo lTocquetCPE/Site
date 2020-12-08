ef8f35d4b3e21ca90f7da9bd462c9d180da37fff
PyrrhaDev <loic.tocquet@gmail.com>
2020-12-08 23:45:05 +0100

    destinations now appear on the home page

--- js/home.js
+++ js/home.js
@@ -2,23 +2,25 @@
 var request = new XMLHttpRequest();
 request.open("GET", "../database/database.json", true);
 request.onreadystatechange = ()=> {
-  if(request.readyState == 4 && request.status ==200){
+  if(request.readyState == 4 && request.status == 200){
     var siteData = JSON.parse(request.responseText);
 
-
+    console.log(siteData.destinationsList)
 
     siteData.destinationsList.forEach((dest)=>{
     //Un peu de babelJS :)
-      document.getElementById("destinations").innerHTML += ```<span class="destination">
+      document.getElementById("destinations").innerHTML += `<span class="destination">
       <h1>${dest.name}</h1>
       <p>Température</p>
       <div class="image">
-        <img src="/ressources/${dest.pictureFileName}" alt="destination" class="image" width=500 height=500/>
+      <a href="/html/reservation.html?dest=${ dest.name}">
+        <img src="/ressources/${ dest.pictureFileName}" alt="destination" class="image" width=500 height=500/>
+        <a/>
         <div class="reservez">
           <h2>Réservez</h2>
         </div>
       </div>
-    </span>```
+    </span>`
     
     })
     
