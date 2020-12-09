653823e4057e3569adef92146cb06dccb856817b
PyrrhaDev <loic.tocquet@gmail.com>
2020-12-09 09:43:30 +0100

    Home.html destination filter added

--- js/home.js
+++ js/home.js
@@ -1,13 +1,38 @@
 
-var request = new XMLHttpRequest();
-request.open("GET", "../database/database.json", true);
-request.onreadystatechange = ()=> {
-  if(request.readyState == 4 && request.status == 200){
-    var siteData = JSON.parse(request.responseText);
 
-    console.log(siteData.destinationsList)
 
-    siteData.destinationsList.forEach((dest)=>{
+let displayDestinations = (siteData) =>{
+
+  let regionRadios = document.filterForm.regionRadios
+  let priceFilter = document.filterForm.rangePrix.value
+  let region = "all"
+  regionRadios.forEach((rad) =>{
+    if(rad.checked)
+    {
+      region = rad.value
+    }
+  })
+
+  let filteredDests = siteData.destinationsList.filter((elem)=>{
+
+    if(elem.displayedPrice > priceFilter)
+    {
+      return false
+    }
+    if(region == "all")
+    {
+      return true
+    }
+    else if(elem.region == region)
+    {
+      return true
+    }
+
+    return false
+  })
+
+  document.getElementById("destinations").innerHTML = ""
+  filteredDests.forEach((dest)=>{
     //Un peu de babelJS :)
       document.getElementById("destinations").innerHTML += `<span class="destination">
       <h1>${dest.name}</h1>
@@ -23,6 +48,32 @@ request.onreadystatechange = ()=> {
     </span>`
     
     })
+  }
+
+
+var request = new XMLHttpRequest();
+request.open("GET", "../database/database.json", true);
+request.onreadystatechange = ()=> {
+  if(request.readyState == 4 && request.status == 200){
+    var siteData = JSON.parse(request.responseText);
+
+    console.log(siteData.destinationsList)
+
+    //EVENTS
+    document.filterForm.regionRadios.forEach(regionRadio =>{
+      regionRadio.addEventListener('change', ()=>{
+
+        displayDestinations(siteData)
+      })
+    })
+    
+    document.filterForm.rangePrix.onchange = ()=>{
+      document.getElementById("priceFilterText").innerHTML = "Prix : " + document.filterForm.rangePrix.value
+      displayDestinations(siteData)
+    }
+
+    displayDestinations(siteData)
+    
     
 
 
