8e820ce0e619f938e705b1c90a29583bb6a26da1
PyrrhaDev <loic.tocquet@gmail.com>
2020-12-10 07:50:48 +0100

    yes

---
+++ js/recapitulatif.js
@@ -0,0 +1,87 @@
+//Finds the current destination from the database
+let getCurrentDestination = (siteData, destinationName) =>{
+  return currentDest = siteData.destinationsList.find((dest) =>(
+    dest.name == destinationName
+  ))
+}
+
+let getDestinationTemperature = (siteData, currentDestination) =>{
+  return new Promise((resolve, reject) => {
+
+    let city= currentDestination.city
+    let cityTempURL="http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=c64e356abc8cd3398484c5fdf9253f87"
+    let destinationTemperature = 20
+    // recupération de la température
+    fetch(cityTempURL)
+    .then (function(reponse){
+        if (reponse.ok){
+            reponse.json().then(re=>{
+            resolve(re.main.temp - 273,15)
+            }).catch(console.error)
+        }else{
+            console.log("Mauvaise réponse du serveur")
+        }
+    })
+    .catch(console.error);  
+
+  })
+}
+
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const destinationName = urlParams.get('destinationName')
+const nom = urlParams.get('nom')
+const prenom = urlParams.get('prenom')
+const adultsNb = urlParams.get('adultsNb')
+const childrenNb = urlParams.get('childrenNb')
+console.log(destinationName)
+
+
+
+//recupération liste des destination
+
+var request = new XMLHttpRequest();
+request.open("GET", "../database/database.json", true);
+request.onreadystatechange = ()=> {
+  if(request.readyState == 4 && request.status == 200){
+    var siteData = JSON.parse(request.responseText);
+
+    let currentDestination = getCurrentDestination(siteData, destinationName)
+    let currentTemperature = -273.15
+    console.log(currentDestination)
+    //EVENTS
+
+
+
+    //API 
+    getDestinationTemperature(siteData, currentDestination).then((temp)=>{
+      currentTemperature = temp
+      console.log(currentTemperature)
+      document.getElementById("infoTemperature").innerHTML = temp.toFixed(1) + '°C'
+    }
+    ).catch(console.error)
+
+    //Reservation data fillling document
+    document.getElementById("destinationTitle").innerHTML = "Destination : " + currentDestination.name
+    document.getElementById("clientFamilyName").innerHTML = "Nom : "+ nom
+    document.getElementById("clientName").innerHTML = "Prénom : " + prenom
+    document.getElementById("adultsNb").innerHTML = "Adulte(s) : " + adultsNb
+    document.getElementById("childrenNb").innerHTML = "Adulte(s) : " + childrenNb
+    document.getElementById("phoneNumber").innerHTML = "Numéro de téléphone : " + ((urlParams.get('Téléphone') == "")? "Non renseigné" : urlParams.get('Téléphone') )
+    document.getElementById("departureDate").innerHTML = "Date de départ : " + (urlParams.get('departureDate'))
+    document.getElementById("endDate").innerHTML = "Date de fin : " + (urlParams.get('endDate'))
+    document.getElementById("breakfast").innerHTML = "Petit déjeuner : " + ((urlParams.get('breakfast')) ? "Oui" : "Non")
+    document.getElementById("dinner").innerHTML = "Diner : " + ((urlParams.get('dinner')) ? "Oui" : "Non")
+    document.getElementById("animals").innerHTML = "Animaux : " + urlParams.get('animalsNb')
+    document.getElementById("price").innerHTML = "Prix : " + urlParams.get('price') + "€"
+    document.getElementById("reservationNumber").innerHTML = "Numéro de réservation : " + (Math.random()*100000000).toFixed(0  )
+    document.getElementById("remarques").innerHTML = "Remarques : " + (urlParams.get('Remarques') == ""? "Aucune" : urlParams.get('Remarques') )
+
+
+  }
+}
+request.send();
+
+    
+  
--- js/reservation.js
+++ js/reservation.js
@@ -16,47 +16,155 @@ class Destination {
   }
 }
 
-function listDest (pDest){
-  lDest=[]
-  for (i=0;i<pDest.destinationsList.length;i++){
-    lDest.push(new destinationsList(pDest.destinationsList[i]));
-  };
-  console.log(lDest)
-  return lDest
+//Finds the current destination from the database
+let getCurrentDestination = (siteData, destinationName) =>{
+  return currentDest = siteData.destinationsList.find((dest) =>(
+    dest.name == destinationName
+  ))
 }
 
-//recupération liste des destination
+let resetForm = () =>{
+  document.forms.bookingForm.reset()
+}
 
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    var myObj = JSON.parse(this.responseText);
-    //fonction qui créer la liste des destinations
-    var listeDestination=listDest(myObj)
-    console.log(listeDestination)
-  }
-};
-xmlhttp.open("GET", "database.txt", true);
-xmlhttp.send();
-console.log(xmlhttp)
-    
-    // use forEach pour la boucle for
-    //recupération de la ville (pour température)
-    var city="tokyo"
-    var cityTempURL="http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=c64e356abc8cd3398484c5fdf9253f87"
+let getDestinationTemperature = (siteData, currentDestination) =>{
+  return new Promise((resolve, reject) => {
 
+    let city= currentDestination.city
+    let cityTempURL="http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=c64e356abc8cd3398484c5fdf9253f87"
+    let destinationTemperature = 20
     // recupération de la température
     fetch(cityTempURL)
     .then (function(reponse){
         if (reponse.ok){
             reponse.json().then(re=>{
-            console.log(re.main.temp)
-            })
+            resolve(re.main.temp - 273,15)
+            }).catch(console.error)
         }else{
             console.log("Mauvaise réponse du serveur")
         }
     })
-    .catch(function (error) {
-        console.log(error);
-    });  
-//request.send();
+    .catch(console.error);  
+
+  })
+}
+
+let warnUser =( msg) =>
+{
+  alert(msg)
+}
+let displayPrice = (currentDest, form) =>{
+
+  console.log(document.forms.bookingForm.elements.departureDate)
+  let depDate = document.forms.bookingForm.elements.departureDate.valueAsDate
+  let endDate = document.forms.bookingForm.elements.endDate.valueAsDate
+  let difference = endDate.getTime()- depDate.getTime() 
+
+  if(difference <= 0)
+  {
+    warnUser("Votre date de retour doit être après votre date de départ !")
+    return
+  }
+  let days = Math.ceil(difference / (1000 * 3600 * 24));
+console.log(days*(form.elements.breakfast.checked ? 12 : 0))
+  let sum =  (0.40*Number(form.elements.childrenNb.value) + Number(form.elements.adultsNb.value)) * (currentDest.displayedPrice + days*currentDest.perDayPrice + days*(form.elements.breakfast.checked ? 12 : 0) + days*(form.elements.dinner.checked ? 30 : 0)) 
+  document.getElementById("priceDisplay").innerHTML = "Prix : " + sum.toFixed(2) + "€"
+  document.forms.bookingForm.elements.price.value = sum.toFixed(2)
+}
+
+let checkIfSubmittable = () =>{
+  let form = document.forms.bookingForm.elements
+
+  let remainingFields = ""
+  if(!form.nom.value)remainingFields += "Nom\n"
+  if(!form.prenom.value)remainingFields += "Prénom\n"
+  if(!form.mail.value)remainingFields += "E-mail\n"
+
+  if(remainingFields != "")
+  {
+    alert("Certains champs n'ont pas été remplis et doivent l'être pour valider la réservation :\n" + remainingFields);
+    return false
+  }
+    return true
+}
+
+let submitForm = () =>{
+  if(checkIfSubmittable())
+  {
+    document.forms.bookingForm.submit()
+  }
+
+}
+
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const destinationName = urlParams.get('dest')
+console.log(destinationName)
+
+
+
+//recupération liste des destination
+
+var request = new XMLHttpRequest();
+request.open("GET", "../database/database.json", true);
+request.onreadystatechange = ()=> {
+  if(request.readyState == 4 && request.status == 200){
+    var siteData = JSON.parse(request.responseText);
+
+    let currentDestination = getCurrentDestination(siteData, destinationName)
+    let currentTemperature = -273.15
+
+    document.forms.bookingForm.elements.destinationName.value = destinationName
+
+    //EVENTS
+    document.forms.bookingForm.elements.adultsNb.addEventListener("change", ()=>{
+      displayPrice(currentDestination, document.forms.bookingForm)
+    })
+    document.forms.bookingForm.elements.childrenNb.addEventListener("change", ()=>{
+      displayPrice(currentDestination, document.forms.bookingForm)
+    })
+    document.forms.bookingForm.elements.departureDate.addEventListener("change", ()=>{
+      displayPrice(currentDestination, document.forms.bookingForm)
+    })
+    document.forms.bookingForm.elements.endDate.addEventListener("change", ()=>{
+      displayPrice(currentDestination, document.forms.bookingForm)
+    })
+    document.forms.bookingForm.elements.breakfast.addEventListener("change", ()=>{
+      displayPrice(currentDestination, document.forms.bookingForm)
+    })
+    document.forms.bookingForm.elements.dinner.addEventListener("change", ()=>{
+      displayPrice(currentDestination, document.forms.bookingForm)
+    })
+
+
+    //API 
+    getDestinationTemperature(siteData, currentDestination).then((temp)=>{
+      currentTemperature = temp
+      console.log(currentTemperature)
+      document.getElementById("infoTemperature").innerHTML = temp.toFixed(1) + '°C'
+    }
+    ).catch(console.error)
+
+    //Reservation data fillling document
+    document.getElementById("destinationName").innerHTML = currentDestination.name
+    document.getElementById("pricePoint").innerHTML = "A partir de "+currentDestination.displayedPrice+" euros"
+    document.getElementById("travelDescription").innerHTML = currentDestination.description
+
+    document.getElementById("availability").innerHTML = "Disponibilité : " + currentDestination.availability
+    document.getElementById("adultsMax").innerHTML = "Nombre de personnes : "+ currentDestination.adultsMax + " adultes max"
+    document.getElementById("animalsAllowed").innerHTML = "Animaux : " +(currentDestination.animalsAllowed ? "Autorisés" : "Interdits")
+    document.getElementById("breakfast").innerHTML = "Petit Déjeuner : " + (currentDestination.breakfastAvailable?"Oui" : "Non") 
+    document.getElementById("dinner").innerHTML = "Diner : " + (currentDestination.dinnerAvailable ?"Oui" : "Non") 
+    document.getElementById("travelPic").src = "../ressources/" + currentDestination.pictureFileName
+    document.forms.bookingForm.elements.adultsNb.max = currentDestination.adultsMax 
+
+    displayPrice(currentDestination, document.forms.bookingForm)
+
+
+  }
+}
+request.send();
+
+    
+  
