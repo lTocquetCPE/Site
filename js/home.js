a043fabdd6ba8028f16c4126c2d6576db5254b27
Alice Malosse <alice.malosse@cpe.fr>
2020-12-10 09:20:47 +0100

    try home temp js (alice)

--- js/home.js
+++ js/home.js
@@ -31,19 +31,18 @@ let displayDestinations = (siteData) =>{
   document.getElementById("destinations").innerHTML = ""
   filteredDests.forEach((dest)=>{
     //Un peu de babelJS :)
-      document.getElementById("destinations").innerHTML += `<span class="destination">
-      <h1>${dest.name}</h1>
-      <p>Température</p>
-      <div class="image">
-      <a href="/html/reservation.html?dest=${ dest.name}">
-        <img src="/ressources/${ dest.pictureFileName}" alt="destination" class="image" width=500 height=500/>
-        <a/>
-        <div class="reservez">
-          <h2>Réservez</h2>
+    document.getElementById("destinations").innerHTML += `<span class="destination">
+        <h1>${dest.name}</h1>
+        <div id ="infoTemperature" class="travelInfoTemperature"></div>
+        <div class="image">
+        <a href="/html/reservation.html?dest=${ dest.name}">
+          <img src="/ressources/${ dest.pictureFileName}" alt="destination" class="image" width=500 height=500/>
+          <a/>
+          <div class="reservez">
+            <h2>Réservez</h2>
+          </div>
         </div>
-      </div>
-    </span>`
-    
+      </span>`
     })
   }
 
@@ -77,3 +76,41 @@ request.onreadystatechange = ()=> {
   }
 }
 request.send();
+
+// récupération de la température
+let getCurrentDestination = (siteData, destinationName) =>{
+  return currentDest = siteData.destinationsList.find((dest) =>(
+    dest.name == destinationName
+  ))
+}
+
+let getDestinationTemperature = (siteData, DestinationName) =>{
+  return new Promise((resolve, reject) => {
+
+    let city= getCurrentDestination(siteData,DestinationName).city
+    let cityTempURL="http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=c64e356abc8cd3398484c5fdf9253f87"
+    let destinationTemperature = 20
+    // recupération de la température
+    fetch(cityTempURL)
+    .then (function(reponse){
+        if (reponse.ok){
+            reponse.json().then(re=>{
+            resolve(re.main.temp - 273,15)
+            }).catch(console.error)
+        }else{
+            console.log("Mauvaise réponse du serveur")
+        }
+    })
+    .catch(console.error);  
+
+  })
+}
+
+filteredDests.forEach((dest)=>{
+  getDestinationTemperature(siteData, dest.name).then((temp)=>{
+    currentTemperature = temp
+    console.log(currentTemperature)
+    document.getElementById("infoTemperature").innerHTML = temp.toFixed(1) + '°C'
+  }
+  ).catch(console.error)
+})
